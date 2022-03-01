80038bae1aa0e5ec7605476ce171962fce769895
KirrimK <KirrimK@users.noreply.github.com>
2022-03-01 21:04:29 +0100

    ajouté les pins potentiels pour le servo poelon

--- src/config.h
+++ src/config.h
@@ -28,7 +28,7 @@ const int MOT1_DIR = PB2;//33;34
 const int MOT2_PWM = PA8;//35;
 const int MOT2_DIR = PB10;//34;
 
-const int POELON_SERVO_PIN = 0; //TODO: mettre le bon pin
+const int POELON_SERVO_PIN = 0; //TODO: mettre le bon pin (PA0, PA1 ou PB0)
 const int POELON_RETRACTED_ANGLE = 0; //TODO: mettre le bon angle
 const int POELON_DEPLOYED_ANGLE = 0; //TODO: mettre le bon angle
 const int POELON_PUSH_ANGLE = 0; //TODO: mettre le bon angle
