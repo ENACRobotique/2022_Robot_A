2dadabaee13f659cf036d0c8d251eb3ceffc74e8
jonathanTIE <jonathan_bleu@hotmail.fr>
2022-03-01 21:49:39 +0100

    pseudo code test hardware

---
+++ src/test_benchmark.cpp
@@ -0,0 +1,20 @@
+#include "TestHardware.h"
+/*
+class MotorTest : public TestHardware
+{
+    MotorTest(float seconds_interval) {
+        //this->seconds_interval = seconds_interval;
+    }
+    void launch_test(float *x, float *y) {
+        //if encoder has advanced ..
+    }
+};
+namespace test_benchmark {
+
+    
+    MotorTest motor_test = MotorTest(2.0f);
+    #motor_test.launch_test();
+        
+
+}
+*/
